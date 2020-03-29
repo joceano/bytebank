41b9a94ff7aef89ec7085e1968d260f59cbdb948
Joceano <alves.joceano@gmail.com>
2020-03-29 12:50:28 -0300

    Definindo o tema do projeto.

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,15 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: ListaTransferencias(),
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        )
       ),
+      home: ListaTransferencias(),
     );
   }
 }
@@ -119,7 +125,9 @@ class ListaTransferenciasState extends State<ListaTransferencias> {
           }));
           future.then((transferenciaRecebida) {
             setState(() {
-              widget._transferencias.add(transferenciaRecebida);
+              if (transferenciaRecebida != null) {
+                widget._transferencias.add(transferenciaRecebida);
+              }
             });
           });
         },
